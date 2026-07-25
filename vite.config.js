5bd91b73f6fa4db610002e60c9bdb5d163d04279
BlackVault Universe <support@enterblackvault.com>
2026-07-25 00:06:22 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         home: resolve(__dirname, 'home.html'),
+        nexus: resolve(__dirname, 'nexus.html'),
       },
     },
   },
