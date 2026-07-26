6108d40bc43c025dc92efa560066f519fe83c368
BlackVault Universe <support@enterblackvault.com>
2026-07-26 15:00:55 -0600

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     host: true,
     allowedHosts: true,
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        home: resolve(__dirname, 'home.html'),
-        nexus: resolve(__dirname, 'nexus.html'),
-      },
-    },
   },
 });
